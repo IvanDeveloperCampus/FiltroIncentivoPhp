ce6af13329d19ea632a796de51798aebeb509818
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-28 08:29:49 -0500

    feat: :sparkles: config clase database

---
+++ src/config/Constants.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Spukm01069\Backend\config;
+
+class Constants{
+
+    static public string $HOST = 'localhost';
+    static public string $DB = 'campuslands';
+    static public string $USER = 'campus';
+    static public string $PASSWORD = 'campus2023';
+    static public string $CHARSET = 'utf8mb4';
+}
+
+?>
