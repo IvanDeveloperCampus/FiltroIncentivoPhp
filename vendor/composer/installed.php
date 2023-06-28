7bb5ee0a145e86a03b4416eaab1be57e4a6c8b4f
IvanDeveloperCampus <ivancampus2023@gmail.com>
2023-06-28 08:29:29 -0500

    feat: :tada: Instalacion composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'spukm01-069/backend',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '13cf86c05748c6f81aa7e44282413e2d54de8598',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bramus/router' => array(
+            'pretty_version' => '1.6.1',
+            'version' => '1.6.1.0',
+            'reference' => '55657b76da8a0a509250fb55b9dd24e1aa237eba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bramus/router',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'spukm01-069/backend' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '13cf86c05748c6f81aa7e44282413e2d54de8598',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
